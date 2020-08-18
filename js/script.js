01b5741893a13dfb999bebd55ff705eb345299e4
Daniel <danielfunnell@yahoo.co.uk>
2020-08-18 19:06:00 +0100

    Built get results function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const questDiv= document.getElementById("question-container");
+const resultsContainer = document.getElementById("results");
+let score = 0;
 
-document.getElementById("btn-start").addEventListener("click", buildQuiz);
+document.getElementById("btn-start").addEventListener("click", startOne);
+document.getElementById("submit").addEventListener("click", getResults);
 
 let questions = [
     {
@@ -23,20 +26,36 @@ let questions = [
 //  });
 
 
-function buildQuiz() {
-    for(i = 0; i < questions.length; i++) {
-    questDiv.innerHTML += `<h4> ${questions[i].question} </h4>`;
-
-    let answers = questions[i].choices;
-     for(j = 0; j < answers.length; j++) {
-          console.log(answers[j]);
-          questDiv.innerHTML += ` <label><input type="radio">${answers[j]}</label>`;
-     }
-}
+function startOne() {
+    //for(i = 0; i < questions.length; i++) {
+    //let quest = questions[i].question
+        
+          questDiv.innerHTML += `<h4> ${questions[0].question} </h4>`;
+          let answers = questions[0].choices;
+          questDiv.innerHTML += ` <label><input type="radio" name="answer-btn" id="beginner" value="start">${answers[0]}</label>`;
+          questDiv.innerHTML += ` <label><input type="radio" name="answer-btn" id="intermediate" value="middle">${answers[1]}</label>`;
+          questDiv.innerHTML += ` <label><input type="radio" name="answer-btn" id="advance" value="end">${answers[2]}</label>`;
+        
 };
 
 
+ function getResults() {
+   document.getElementsByName("answer-btn")
+    .forEach(radio => {
+            if ((radio.checked) && (radio.value == "start")) {
+            console.log(radio.value) 
+            score = score +1;  
+            } else if ((radio.checked) && (radio.value == "middle")) {
+            score = score +2;
+            } else if ((radio.checked) && (radio.value == "end")) {
+                score = score +3;
+            }
+        });
 
+   
+    
+
+};
 
 
 

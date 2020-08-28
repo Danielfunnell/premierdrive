a00a6d21ce4a06e1f9c8056dadefe38378609636
Daniel <danielfunnell@yahoo.co.uk>
2020-08-28 16:10:58 +0100

    updated course finder application, added name and email form and stopped page reloading on submit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 const questDiv= document.getElementById("question-container");
 const resultsContainer = document.getElementById("results");
+let formOne = document.getElementById("form").addEventListener("submit", handleform)
 
+function handleform(event) {
+    event.preventDefault();
+    form.parentNode.removeChild(form);
+    renderQuestion();
+}
 
-let score = [];
+const next= document.querySelector(".next-button");
+const submit = document.getElementById("submit").addEventListener("click", getResults);
+
+let score = 0
 let occurances = {};
 let curNumber;
-let maxNumber = score[0];
+// let maxNumber = score[0];
+
 
 //document.getElementById("btn-start").addEventListener("click", start);
-document.getElementById("next").addEventListener("click", nextQuest);
-let submit = document.getElementById("submit").addEventListener("click", getResults);
+//document.getElementById("next").addEventListener("click", nextQuest);
+// form.addEventListener("click", renderQuestion);
+
+
 
 let questions = [
     {
@@ -33,26 +45,40 @@ let questions = [
     {
         question: "Do you have your own car to practice in as well?",
         choiceA: "No",
-        choiceB: "Maybe",
+        choiceB: "Occasionally",
         choiceC: "Yes"
     }
 ];
 
+
 let lastQuestionIndex = questions.length -1; 
 let runningQuestionIndex = 0;
+const questOne = questions[1];
 
+quizStart();
+    
+function quizStart (event) {
+    //  event.preventDefault()
+   
+    next.style.display='none';
 
-renderQuestion()
+}
+//renderQuestion();
 
 
 function renderQuestion() {
+        
+       
+        next.style.display='block';
+    
    
         let q = questions[runningQuestionIndex];
-        questDiv.innerHTML += `<h4> ${q.question} </h4>`;
+        questDiv.innerHTML += `<h4 class="question-box__question my-0 font-weight-normal"> ${q.question} </h4>`;
         questDiv.innerHTML += `<label><input type="radio" name="answer-btn" id="A" value="start">${q.choiceA}</label></br>`
         questDiv.innerHTML += `<label><input type="radio" name="answer-btn" id="B" value="middle">${q.choiceB}</label><br>`
         questDiv.innerHTML += `<label><input type="radio" name="answer-btn" id="C" value="end">${q.choiceC}</label><br>`
-         
+        
+        
 };
 
 
@@ -67,7 +93,7 @@ function nextQuest(){
     } if (runningQuestionIndex === lastQuestionIndex) {
         document.getElementById("question-container").style.display='none';
         document.getElementById("submit").style.display='none'
-        getScore();
+        displayResult();
     }
     else {
         runningQuestionIndex++;
@@ -81,19 +107,29 @@ function nextQuest(){
  function getResults() {
    document.getElementsByName("answer-btn")
     .forEach(radio => {
+           
+        
             if ((radio.checked) && (radio.value === "start")) {
-                score.push(radio.value); 
+                score = score -1; 
+                console.log(score)
                 nextQuest()
             } else if ((radio.checked) && (radio.value === "middle")) {
-                score.push(radio.value);
+                score ++
+                console.log(score)
                 nextQuest()
             } else if ((radio.checked) && (radio.value === "end")) {
-                score.push(radio.value);
+                score = score +1
                 nextQuest()
                 console.log(score)
+                
             }
+
+            
         });
+
+        
 };
+
 
 
     // function getScore () {
@@ -108,34 +144,37 @@ function nextQuest(){
 
 
 
-function getScore() {
-for(let i = 0; i < score.length; i++) {
-    curNumber = score[i];
+// function getScore() {
+// for(let i = 0; i < score.length; i++) {
+//     curNumber = score[i];
 
-    if(occurances[curNumber] !== undefined) {
-        occurances[curNumber]++;
-    } else {
-        occurances[curNumber] = 1;
-    }
-    if(occurances[curNumber] > occurances[maxNumber]) {
-        maxNumber = curNumber;
-    }
-}
-    console.log(curNumber);
-    console.log(maxNumber);
-    displayResult();
-};
+//     if(occurances[curNumber] !== undefined) {
+//         occurances[curNumber]++;
+//     } else {
+//         occurances[curNumber] = 1;
+//     }
+//     if(occurances[curNumber] > occurances[maxNumber]) {
+//         maxNumber = curNumber;
+//     }
+// }
+
+
+//     displayResult();
+// };
+
+
 
 
 function displayResult() {
-    if (curNumber == "start") {
+
+    if (score < 0) {
         resultsContainer.innerHTML =`<h4> You are a beginner. We recommend our 40 hour beginner course </h4>`;
         console.log("you are a beginner");
-    } if (curNumber == "middle") {
+    }else if ((score >= 0) && (score <= 3)) {
         resultsContainer.innerHTML =`<h4> You are a intermediate. We recommend our 20 hour course </h4>`;
         console.log("you are intermediate");
     }
-    else if (curNumber == "end") {
+    else if (score >= 4) {
         resultsContainer.innerHTML =`<h4> You are advanced. We recommend our 10 hour course </h4>`;
         console.log("you are advanced");
     } 

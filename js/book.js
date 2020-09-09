9c30c99e50ef3e6402ba5a98d1bfe4fa90ad31e9
Daniel <danielfunnell@yahoo.co.uk>
2020-09-09 12:19:20 +0100

    got book online form validation functionality working

--- js/book.js
+++ js/book.js
@@ -1,50 +1,76 @@
-const previous = document.getElementById("prevBtn");
+
+// targets next and previous buttons
+const previous = document.getElementById("prevBtn")
 const next = document.getElementById("nextBtn")
-next.addEventListener("click", nextForm);
+
+
+// click events added to next and previous buttons
+previous.addEventListener("click", prevForm);
+next.addEventListener("click", handleform);
+
+// tragets tab divs
 let tab = document.getElementsByClassName("tab");
 let currentTab = 0;
 
+// form validation
+function handleform() {
 
+    let tab = document.getElementsByClassName("tab");
+    let input, i
+    
+    input = tab[currentTab].getElementsByTagName("input");
+    
+    // loops through input fields to check content and returns false if content is empty otherwise moves to next form.
+   for(i=0; i < input.length; i++) {
+        if(input[i].value == "") {
+        return false
+          
+        // if on last tab and input fields are valid changes next button to type submit
+        } if (currentTab == tab.length-1) {
+            next.type = "submit"
+        }
+    }
 
+    nextForm()
+}
+
+//displays current form
 showTab(currentTab);
-
-
 
 
 function showTab(currentTab) {
     
-
     tab[currentTab].style.display = "block";
 
     if(currentTab == 0) {
         previous.style.display = "none" 
-    
     } else {
         previous.style.display = "inline-block"
-    }
-     if (currentTab == (tab.length -1)) {
+    } if  (currentTab == tab.length-1) {
         next.innerHTML = "Submit";
-        next.removeEventListener("click", nextForm);
-        next.addEventListener("click", changeSubmit)
-        function changeSubmit() {
-            next.type = "submit";
-        }
-      } else {
+    } else {
         next.innerHTML = "Next";
-        
     }
-
 }
 
+// handles next button
 function nextForm () {
-    let lastForm = tab[currentTab]
+  let lastForm = tab[currentTab]
 
-    currentTab++;
-    console.log(currentTab)
-    showTab(currentTab);
-    lastForm.style.display = "none"
-    console.log(lastForm)
+        currentTab++;
+        showTab(currentTab);
+        lastForm.style.display = "none";
 }
+
+//handles previous button
+function prevForm () {
+
+    let lastForm = tab[currentTab];
+    lastForm.style.display = "none";
+    currentTab--;
+    showTab(currentTab);
+}
+
 
 
 
